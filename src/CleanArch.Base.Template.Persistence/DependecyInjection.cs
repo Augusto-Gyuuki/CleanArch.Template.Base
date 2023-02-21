47202c3cf50bd6aa244718c2d238c04b61cb513a
Augusto Almeida <59775769+Augusto-Gyuuki@users.noreply.github.com>
2023-02-21 16:34:24 -0300

    changing folder struct

---
+++ src/CleanArch.Base.Template.Persistence/DependecyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CleanArch.Base.Template.Persistence;
+
+[ExcludeFromCodeCoverage]
+public static class DependecyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection service)
+    {
+        return service;
+    }
+}
--- tests/CleanArch.Base.Template.Tests/ArchitectureTests.cs
+++ tests/CleanArch.Base.Template.Tests/ArchitectureTests.cs
@@ -1,6 +1,6 @@
 ﻿using NetArchTest.Rules;
 
-namespace CleanArch.Base.Template.Tests;
+namespace CleanArch.Base.Template.Tests.Unit;
 
 public sealed class ArchitectureTests
 {
@@ -8,9 +8,11 @@ public sealed class ArchitectureTests
     private const string APPLICATION_NAMESPACE = "Application";
     private const string INFRASTRUCTURE_NAMESPACE = "Infrastructure";
     private const string PRESENTATION_NAMESPACE = "Presentation";
+    private const string PERSISTENCE_NAMESPACE = "Persistence";
     private const string WEB_NAMESPACE = "Web";
 
     [Fact(DisplayName = "Domain Layer Should Not Have Dependency on Others Projects")]
+    [Trait("Architecture", "Domain")]
     public void Domain_ShouldNotHaveDependencyOnOthersProjects()
     {
         //Arrange
@@ -22,6 +24,7 @@ public sealed class ArchitectureTests
             INFRASTRUCTURE_NAMESPACE,
             PRESENTATION_NAMESPACE,
             WEB_NAMESPACE,
+            PERSISTENCE_NAMESPACE,
         };
 
         //Act
@@ -36,6 +39,7 @@ public sealed class ArchitectureTests
     }
 
     [Fact(DisplayName = "Application Layer Should Not Have Dependency on Others Projects")]
+    [Trait("Architecture", "Application")]
     public void Application_ShouldNotHaveDependencyOnOthersProjects()
     {
         //Arrange
@@ -46,6 +50,7 @@ public sealed class ArchitectureTests
             INFRASTRUCTURE_NAMESPACE,
             PRESENTATION_NAMESPACE,
             WEB_NAMESPACE,
+            PERSISTENCE_NAMESPACE,
         };
 
         //Act
@@ -59,7 +64,26 @@ public sealed class ArchitectureTests
         result.IsSuccessful.Should().BeTrue();
     }
 
+    //[Fact(DisplayName = "Application Layer Should Have Dependency on Domain Project")]
+    //[Trait("Architecture", "Application")]
+    //public void Application_ShouldHaveDependencyOnDomainProject()
+    //{
+    //    //Arrange
+    //    var assembly = typeof(Application.AssemblyReference).Assembly;
+
+    //    //Act
+    //    var result = Types
+    //        .InAssembly(assembly)
+    //        .Should()
+    //        .HaveDependencyOn(DOMAIN_NAMESPACE)
+    //        .GetResult();
+
+    //    //Asserts
+    //    result.IsSuccessful.Should().BeTrue();
+    //}
+
     [Fact(DisplayName = "Handlers Should Have Dependency on Domain")]
+    [Trait("Architecture", "Application")]
     public void Handlers_ShouldHaveDependencyOnDomain()
     {
         //Arrange
@@ -79,6 +103,7 @@ public sealed class ArchitectureTests
     }
 
     [Fact(DisplayName = "Infrastructure Layer Should Not Have Dependency on Others Projects")]
+    [Trait("Architecture", "Infrastructure")]
     public void Infrastructure_ShouldNotHaveDependencyOnOthersProjects()
     {
         //Arrange
@@ -88,6 +113,32 @@ public sealed class ArchitectureTests
         {
             PRESENTATION_NAMESPACE,
             WEB_NAMESPACE,
+            PERSISTENCE_NAMESPACE,
+        };
+
+        //Act
+        var result = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        //Asserts
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact(DisplayName = "Persistence Layer Should Not Have Dependency on Others Projects")]
+    [Trait("Architecture", "Persistence")]
+    public void Persistence_ShouldNotHaveDependencyOnOthersProjects()
+    {
+        //Arrange
+        var assembly = typeof(Persistence.AssemblyReference).Assembly;
+
+        var otherProjects = new[]
+        {
+            PRESENTATION_NAMESPACE,
+            WEB_NAMESPACE,
+            PERSISTENCE_NAMESPACE,
         };
 
         //Act
@@ -102,6 +153,7 @@ public sealed class ArchitectureTests
     }
 
     [Fact(DisplayName = "Presentation Layer Should Not Have Dependency on Others Projects")]
+    [Trait("Architecture", "Presentation")]
     public void Presentation_ShouldNotHaveDependencyOnOthersProjects()
     {
         //Arrange
@@ -111,6 +163,7 @@ public sealed class ArchitectureTests
         {
             INFRASTRUCTURE_NAMESPACE,
             WEB_NAMESPACE,
+            PERSISTENCE_NAMESPACE,
         };
 
         //Act
@@ -118,25 +171,6 @@ public sealed class ArchitectureTests
             .InAssembly(assembly)
             .ShouldNot()
             .HaveDependencyOnAll(otherProjects)
-            .GetResult();
-
-        //Asserts
-        result.IsSuccessful.Should().BeTrue();
-    }
-
-    [Fact(DisplayName = "Controllers Should Have Dependency on MediatR")]
-    public void Controllers_ShouldHaveDependencyOnMediatR()
-    {
-        //Arrange
-        var assembly = typeof(Presentation.AssemblyReference).Assembly;
-
-        //Act
-        var result = Types
-            .InAssembly(assembly)
-            .That()
-            .HaveNameEndingWith("Controller")
-            .Should()
-            .HaveDependencyOn("MediatR")
             .GetResult();
 
         //Asserts
